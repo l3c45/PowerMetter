af54705bbf7d469b5ace6889802e3e3b786534f1
Lucas Zarate <105124049+l3c45@users.noreply.github.com>
2023-01-09 13:47:04 -0300

    WEB-Añadir reglas eslint en App

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
 import About from "./components/About";
 import ChartGraph from "./components/ChartGraph";
 import Header from "./components/Header";
